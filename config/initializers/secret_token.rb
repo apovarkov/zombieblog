ab3e006a067b0ecbaabf15f78f67466a123ea7ad
a.povarkov <alextaktik@gmail.com>
2013-09-10 09:42:57 +0300

    Project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zombieblog::Application.config.secret_key_base = '9a5bc9b20910d1fdd9dda34992b7d97657430c3ec579627792a9a5b32489a019c93c6eb744125ca4303c3fbc239c031c4a1daf505ea07284aa3214ef954d6a7f'
